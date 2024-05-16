aa8d6b47e5f8d16dd71e5d5be2db5fe2ce9e3512
AlaaAj2001 <114425831+AlaaAj2001@users.noreply.github.com>
2024-05-16 18:57:44 +0300

    Edit the Navbar and make it responsive

--- src/topNavbar/topNavbar.jsx
+++ src/topNavbar/topNavbar.jsx
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartPlus, faRepeat } from "@fortawesome/free-solid-svg-icons";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import "./topNavbar.css";
 
 const TopNavbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <>
-      <div className="icons_div">
-        <FontAwesomeIcon className="icons" icon={faRepeat} />
-        <FontAwesomeIcon className="icons" icon={faHeart} />
-        <FontAwesomeIcon className="icons" icon={faCartPlus} />
-        <div className="cart_div">
-          <p className="your_cart">Your Cart</p>
-          <p className="price">$ 0.00</p>
+      <div className="top_navbar">
+        <div className="icons_div">
+          <FontAwesomeIcon className="icons" icon={faRepeat} />
+          <FontAwesomeIcon className="icons" icon={faHeart} />
+          <FontAwesomeIcon className="icons" icon={faCartPlus} />
+          <div className="cart_div">
+            <p className="your_cart">Your Cart</p>
+            <p className="price">$ 0.00</p>
+          </div>
         </div>
-      </div>
-      <p className="title">CAMARO</p>
-      <div className="bar_div">
+        <p className="title">CAMARO</p>
+        <div className="menu-toggle" onClick={toggleMenu}>
+          <button>☰</button>
+        </div>
+      </div>{" "}
+      <div className={`bar_div ${showMenu ? "show" : ""}`}>
         <button>HOME</button>
         <button>SHOP</button>
         <button>PAGES</button>
